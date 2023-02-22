ee18f75e90530f56f2d4baf5a84fd1a5614ae75d
karimkhaled0 <kkmawe@gmail.com>
2023-02-22 14:53:22 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <div>
+            <h1 className='text-red-500'>Hi</h1>
+        </div>
+    )
+}
+
+export default page
